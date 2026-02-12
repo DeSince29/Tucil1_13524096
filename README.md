431203657724fdb6d0e2dc5cfd46f29a7c599797
DeSince29 <147355738+DeSince29@users.noreply.github.com>
2026-02-13 00:38:34 +0700

    Main Program
    Penyelesaian Program Utama
    
    Req: Bonus hehe

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# Queens
+# Tugas Kecil 1 IF2211 Strategi Algoritma - Penyelesaian Queens LinkedIn
 
+Program Penyelesaian Permainan Queens Linkedin menggunakan Algoritma Brute Force
+
+## Deskripsi Program
+Program ini bertujuan untuk menyelesaikan permainan logika Queens yang sedag populer di LinkedIn. Tujuan utama permainan ini adalah dengan menempatkan N buah Queen pada papan berwarna (dengan jumlah N warna) berukuran N x N dengan aturan sebagai berikut.
+1. Hanya ada satu Queen per baris, kolom, dan daerah warna.
+2. Queen tidak boleh saling bersentuhan, walau secara diagonal.
+
+Program menggunakan algoritma Brute Force juga backtracking murni tanpa heuristik untuk mencari solusi dari papan yang menerima masukan input file ".txt". Program juga memiliki vitur GUI dan juga pengukuran waktu eksekusi dan total kasus yang dicoba.
+
+## Struktur Folder
+Struktur direktori repository ini adalah sebagai berikut.
+Tucil1_NIM/
+├── doc/                # Laporan Tugas Kecil
+├── src/                # Source code program
+│   ├── main.py         # Program Utama
+│   └── gui.py          # Program GUI
+├── test/               # Solusi jawaban dari data uji
+│   ├── test_A.txt
+│   ├── test_B.txt
+│   └── solution_test.txt
+└── README.md           # Penjelasan program
+
+## Requirement
+Program ditulis menggunakan bahasa Python. Berikut adalah persyaratan sistem.
+1. OS: Windows atau Linux.
+2. Bahasa Pemrograman: Python 3
+3. Library: Hanya menggunakan standard library sehingga tidak perlu instalasi tambahan
+   a. os: Manajemen file dan pembersihan layar terminal untuk Update.
+   b. time: Pengukuran waktu eksekusi dan delay visualisasi Update.
+   c. tkinter: Antarmuka Pengguna Grafis (GUI)
+
+## Cara Mengkompilasi Program
+Source Code tidak perlu dikompilasi karena menggunakan bahasa Python.
+
+## Cara Menjalankan Program
+Sebelumnya dipastikan berada di direktori root repository (Tucil1_13524096).
+
+### 1. Menjalankan Mode CLI
+1. Buka terminal
+2. Jalankan perintah berikut.
+   ```bash
+   python src/main.py
+   ```
+3. Masukkan nama file .txt
+4. Program akan menampilkan proses pencarian secara Live dan hasil akhir beserta waktu eksekusi dan total kasus.
+
+## Identitas Pembuat
+Program ini dibuat untuk memenuhi Tugas Kecil 1 Strategi Algoritma oleh:
+
+Nama: Moreno Syawali Ganda Sugita
+NIM: 13524096
+Kelas: K02
---
+++ src/main.py
@@ -0,0 +1,98 @@
+import time
+import os
+
+def ReadFiles(name):
+    try:
+        with open(name, 'r') as file:
+            lines = [line.strip() for line in file if line.strip]
+
+            if not lines:
+                return None, 0, "File kosong"
+            
+            board = [list(line) for line in lines]
+            m = len(board)
+            n = len(board[0])
+            for i, row in enumerate(board):
+                if len(row) != n:
+                    return None, 0, f"Baris {i+1} memiliki panjang berbeda"
+            if m != n:
+                return None, 0, f"Papan berbentuk persegi panjang. Tidak Valid"
+            
+            return board, n, "Berhasil input file"
+    except FileNotFoundError:
+        return None, 0, "File tidak ditemukan"
+
+def IsItLegal(row, col, curr, board):
+    color = board[row][col]
+    for r, c in curr:
+        if c == col or board[r][c] == color:
+            return False
+        if abs(r - row) <= 1 and abs(c - col) <= 1:
+            return False
+    return True
+
+def PrintBoard(hasil, n):
+    for row in hasil:
+        print(" ".join(row))
+    print("")
+
+def Update(row, col, board, n, curr):
+    os.system('cls' if os.name == 'nt' else 'clear')
+    temp = [list(r) for r in board]
+    for r, c in curr:
+        temp[r][c] = '#'
+    temp[row][col] = '?'
+    PrintBoard(temp, n)
+    time.sleep(0.05)
+
+iter = 0
+
+def solve(row, curr, board, n):
+    global iter
+
+    if row == n:
+        return True
+
+    for col in range(n):
+        iter += 1
+        # Update(row, col, board, n, curr)
+        if IsItLegal(row, col, curr, board):
+            curr.append((row, col))
+        
+            if solve(row+1, curr, board, n):
+                return True
+        
+            curr.pop()
+    
+    return False
+
+# MAIN
+name = input("Masukkan nama file .txt: ")
+board, n, statusFile = ReadFiles(name)
+print(statusFile)
+
+if board:
+    start = time.perf_counter()
+    solution = []
+
+    if solve(0, solution, board, n):
+        end = time.perf_counter()
+        print("\nSolusi ditemukan:")
+
+        hasil = [list(row) for row in board]
+        for r, c in solution:
+            hasil[r][c] = '#'
+        PrintBoard(hasil, n)
+
+        print(f"Waktu pencarian: {(end-start) * 1000:2f} ms")
+        print(f"Banyak kasus yang ditinjau: {iter} kasus")
+
+        save = input("Ingin menyimpan solusi? (Ya/Tidak): ")
+        if save.lower() == "ya":
+            output = "solusi_" + name
+            with open(output, "w") as file:
+                for row in hasil:
+                    file.write("".join(row) + "\n")
+            print(f"Solusi telah disimpan di {output}")
+    else:
+        print("Tidak ada solusi yang valid.")
